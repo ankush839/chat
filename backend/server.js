0444bff92ac76ac984054a74b34cf9f85bfeafbc
ANKUSH <ankush83959@gmail.com>
2025-10-19 14:13:10 +0100

    Update CORS origin to production URL

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://chat-6-obw5.onrender.com/',
     credentials:true
 };
 app.use(cors(corsOption)); 
